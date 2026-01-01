147cdcf78837663ed99a506aebce26afd50d186f
Brahmananda Sahoo <sahoobrahmananda2004@gmail.com.com>
2026-01-01 20:44:04 +0530

    feat: Youtube URL to audio file added

---
+++ youtube-audio-generator.py
@@ -0,0 +1,35 @@
+# if needed install required libraries
+# pip install yt-dlp moviepy
+
+import yt_dlp
+from moviepy import AudioFileClip
+import os
+
+def download_youtube_audio(youtube_url, output_path):
+    # Configure yt-dlp options
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': 'temp_audio.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    
+    # Download and convert to MP3
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([youtube_url])
+    
+    # Rename the output file
+    if os.path.exists('temp_audio.mp3'):
+        os.rename('temp_audio.mp3', output_path)
+
+youtube_url = "https://youtube.com/watch?si=_SXsFI8fqcwngzXm&v=j1lTvjmOJbQ"
+
+# In case you want to enter the video to use directly:
+# youtube_url = str(input("Enter the URL of the video you want to 
+#               download: \n>>"))
+
+output_path = 'output_file.mp3'
+download_youtube_audio(youtube_url, output_path)
