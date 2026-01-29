84a1834e5029ba140920768538725107742f5b0e
Brahmananda Sahoo <sahoobrahmananda2004@gmail.com.com>
2026-01-29 21:24:04 +0530

    refactor: move image processing logic into image_to_text function

--- services/image-to-text.py
+++ services/image-to-text.py
@@ -3,14 +3,18 @@ import cv2
 from PIL import Image
 
 import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-img_cv = cv2.imread(r'../temp/img2.jpeg')
+def image_to_text(image_path: str) -> str:
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-# By default OpenCV stores images in BGR format and since pytesseract assumes RGB format,
-# we need to convert from BGR to RGB format/mode:
-img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
-print(pytesseract.image_to_string(img_rgb))
-# OR
-img_rgb = Image.frombytes('RGB', img_cv.shape[:2], img_cv, 'raw', 'BGR', 0, 0)
-print(pytesseract.image_to_string(img_rgb))
+    img_cv = cv2.imread(r'../temp/img2.jpeg')
+
+    # By default OpenCV stores images in BGR format and since pytesseract assumes RGB format,
+    # we need to convert from BGR to RGB format/mode:
+    img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+    print(pytesseract.image_to_string(img_rgb))
+    # OR
+    img_rgb = Image.frombytes('RGB', img_cv.shape[:2], img_cv, 'raw', 'BGR', 0, 0)
+    # print(pytesseract.image_to_string(img_rgb))
+    text = pytesseract.image_to_string(img_rgb)
+    return text
